33bb4d46b7466db907734841080c935f6fa81a7a
aasmpro <aa.smpro@gmail.com>
2019-07-13 03:18:01 +0430

    redesign is here

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,8 @@
 <style>
+html {
+    overflow-x: hidden;
+}
+
 body {
 	font-family:Arial, Helvetica, sans-serif;
 	font-size:small;
@@ -10,63 +14,102 @@ body {
 	height:50px;
 	width:50px;
 }
+
+#play-buttons {
+    padding-top: 10px;
+}
+
+#play-buttons button {
+    width: 40px;
+    height: 40px;
+    margin: 3px 21px;
+}
+
+textarea, textarea:focus, button:focus {
+    border: none !important;
+    box-shadow: none !important;
+}
 </style>
 
 <template>
-    <div>
-        <table id="board" v-html="gameBoard()">
-            {{gameBoard()|html}}
-        </table>
-        <button v-on:click="show=!show">
-            {{ show ? 'Hide' : 'Show' }}
-        </button>
-        <p>Arrows: {{arrowsLeft}}</p>
-        <p>Position: {{position}}</p>
-        <p>Sensors: {{sensors}}</p>
-        <p>message: {{message}}</p>
-        <div v-if="controls">
-            <form onsubmit="return false;">
-                <table>
-                    <tr>
-                        <td rowspan="3"><button v-on:click="move('left')">&lt;</button></td>
-                        <td><button v-on:click="move('up')">^</button></td>
-                        <td rowspan="3"><button v-on:click="move('right')">&gt;</button></td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: center">Move</td>
-                    </tr>
-                    <tr>
-                        <td><button v-on:click="move('down')">v</button></td>
-                    </tr>
-                </table>
-            </form>
-            <form onsubmit="return false;">
-                <table>
-                    <tr>
-                        <td rowspan="3"><button v-on:click="shoot('left')">&lt;</button></td>
-                        <td><button v-on:click="shoot('up')">^</button></td>
-                        <td rowspan="3"><button v-on:click="shoot('right')">&gt;</button></td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: center">Shoot</td>
-                    </tr>
-                    <tr>
-                        <td><button v-on:click="shoot('down')">v</button></td>
-                    </tr>
-                </table>
-            </form>
-            <textarea v-model="code"></textarea>
-            <button v-on:click="runCode()">Run</button>
-        </div>
-        <div v-if="!controls">
-            <button v-on:click="reload()">Play Again</button>
-        </div>
-    </div>
+    <b-container class="my-5">
+        <b-row align-h="center">
+            <h2>Hunt The Wumpus</h2>
+        </b-row>
+        <b-row align-h="center">
+            <table id="board" v-html="gameBoard()" v-on:click="show=!show">
+                {{gameBoard()|html}}
+            </table>
+        </b-row>
+        <b-row class="mt-3">
+            <b-col md="4">
+                <span>Arrows: {{arrowsLeft}}</span>
+            </b-col>
+            <b-col md="4">
+                <span>Position: {{position}}</span>
+            </b-col>
+            <b-col md="4">
+                <span>Sensors: {{sensors}}</span>
+            </b-col>
+        </b-row>
+        <b-row class="mb-3">
+            <b-col cols="12">
+                <span>message: {{message}}</span>
+            </b-col>
+        </b-row>
+        <b-row v-if="controls">
+            <b-col cols="12">
+                <b-tabs fill>
+                    <b-tab title="Play" active>
+                        <b-row id="play-buttons">
+                            <b-col cols="6">
+                                <b-row align-h="center">
+                                    <b-button variant="primary" v-on:click="move('up')"><font-awesome-icon icon="chevron-up" /></b-button>
+                                </b-row>
+                                <b-row align-h="center">
+                                    <b-button variant="primary" v-on:click="move('left')"><font-awesome-icon icon="chevron-up" rotation="270" /></b-button>
+                                    <b-button variant="primary" v-on:click="move('right')"><font-awesome-icon icon="chevron-up" rotation="90" /></b-button>
+                                </b-row>
+                                <b-row align-h="center">
+                                    <b-button variant="primary" v-on:click="move('down')"><font-awesome-icon icon="chevron-up" rotation="180" /></b-button>
+                                </b-row>
+                            </b-col>
+                            <b-col cols="6">
+                                <b-row align-h="center">
+                                    <b-button variant="danger" v-on:click="shoot('up')"><font-awesome-icon icon="long-arrow-alt-up" /></b-button>
+                                </b-row>
+                                <b-row align-h="center">
+                                    <b-button variant="danger" v-on:click="shoot('left')"><font-awesome-icon icon="long-arrow-alt-up" rotation="270" /></b-button>
+                                    <b-button variant="danger" v-on:click="shoot('right')"><font-awesome-icon icon="long-arrow-alt-up" rotation="90" /></b-button>
+                                </b-row>
+                                <b-row align-h="center">
+                                    <b-button variant="danger" v-on:click="shoot('down')"><font-awesome-icon icon="long-arrow-alt-up" rotation="180" /></b-button>
+                                </b-row>
+                            </b-col>
+                        </b-row>
+                    </b-tab>
+                    <b-tab title="Code">
+                        <b-form-textarea
+                            id="textarea-no-resize"
+                            placeholder="Enter your code here"
+                            rows="5"
+                            no-resize
+                            v-model="code"
+                        ></b-form-textarea>
+                        <b-button block variant="primary" v-on:click="runCode()">Run</b-button>
+                    </b-tab>
+                </b-tabs>
+            </b-col>
+        </b-row>
+        <b-row v-if="!controls">
+            <b-button block variant="primary" v-on:click="reload()">Play Again</b-button>
+        </b-row>
+    </b-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { GamePiece, GameBoard, Hero, Wumpus, Pit, Bats, Arrow } from "./game"
+import { GamePiece, GameBoard, Hero, Wumpus, Pit, Bats, Arrow } from "./game";
 
 export default Vue.extend({
     data(){
@@ -107,9 +150,9 @@ export default Vue.extend({
                 table += "<tr>";
                 for(var j=0; j<this.height; j++){
                         if(this.board.space[j][i].includes('H')){
-                            table += "<td style=\"background-color: red;\">";
+                            table += "<td style=\"background-color: #007bff;\">";
                         } else if(this.board.space[j][i].includes('X')){
-                            table += "<td style=\"background-color: pink;\">";
+                            table += "<td style=\"background-color: #dc3545;\">";
                         } else {
                             table += "<td>"
                         }
@@ -124,7 +167,6 @@ export default Vue.extend({
         },
 
         init(){
-            this.code = "";
             this.width = 5;
             this.height = 5;
             this.arrowsLeft = 5;
@@ -146,7 +188,7 @@ export default Vue.extend({
         },
 
         win() {
-            this.message = "Win! You Hunted The Wumpus :)";
+            this.message = "Win! You Hunted The Wumpus!";
             this.controls = false;
         },
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "./app.vue"
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronUp, faLongArrowAltUp, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faChevronUp, faLongArrowAltUp, faEye, faEyeSlash)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.config.productionTip = false
+
 export let obj = new Vue({
     el:"#app",
     data:{
